fa19677c5c5e5092874e037f8590c0f4889c50ae
B3TA-BLOCKER <hassaanalibukhari@gmail.com>
2026-05-31 02:29:45 +0500

    added a tab "Dev-Notes"

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Projects' },
+  { href: '/dev-notes', title: 'Dev Notes' },
   { href: '/about', title: 'About' },
 ]
 
