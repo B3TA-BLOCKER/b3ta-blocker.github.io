2051127d8c66503b121cf0dd98e8321ae8485517
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2026-06-02 02:09:04 +0500

    Refactor ListLayoutWithTags to remove header
    Removed the header section and adjusted padding in the layout.

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -79,11 +79,7 @@ export default function ListLayoutWithTags({
   return (
     <>
       <div>
-        <div className="pt-6 pb-6">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            {title}
-          </h1>
-        </div>
+        <div className="pt-2 pb-2" />
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {displayPosts.map((post) => {
             const { path, date, title, summary, tags, images } = post
