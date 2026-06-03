aca449c4b643eb41ce0c1026bc325be6feff0a59
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2026-06-03 10:09:52 +0500

    Import Pre component from local path

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,11 +1,11 @@
 import TOCInline from 'pliny/ui/TOCInline'
-import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import LinkPreview from './LinkPreview'
 import ImageLightbox from './ImageLightbox'
+import Pre from './Pre'
 
 const MdxImage = (props: any) => (
   <ImageLightbox
