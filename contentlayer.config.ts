accb97f054e609c713e7a060d2331263a2c7c4a4
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2026-07-10 04:18:54 +0500

    Add twitterUrl field to content layer config

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -105,6 +105,7 @@ export const Blog = defineDocumentType(() => ({
     layout: { type: 'string' },
     bibliography: { type: 'string' },
     canonicalUrl: { type: 'string' },
+    twitterUrl: { type: 'string' },
   },
   computedFields: {
     ...computedFields,
