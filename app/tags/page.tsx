5d668f92bb4fddfa81564569a70203fc0f0cde6d
B3TA-BLOCKER <hassaanalibukhari@gmail.com>
2026-07-12 03:57:16 +0500

    aligned the tag heading in the tags page

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -109,8 +109,8 @@ export default async function Page() {
   sections.forEach((s) => s.tags.sort((a, b) => tagCounts[b] - tagCounts[a]))
 
   return (
-    <div className="md:mt-24">
-      <div className="space-x-2 pt-6 pb-10">
+    <div>
+      <div className="space-y-2 pt-6 pb-8">
         <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
           Tags
         </h1>
